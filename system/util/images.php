019630b254663532523a257b919742c0f745091a
Eero Laine <horridparlormaster@gmail.com>
2024-05-13 10:23:36 +0300

    Thumbnail folders should now be correct

--- system/util/images.php
+++ system/util/images.php
@@ -92,7 +92,7 @@ function updateThumbnail(
     }
     $smallImg = imagescale($croppedImg, RESULT_SCALE * IMAGE_WIDTH, RESULT_SCALE * IMAGE_HEIGHT);
 
-    $smallPath = getThumbnailPath($ownerId, $cardId, $imageName);
+    $smallPath = getThumbnailFolderPath($ownerId, $cardId);
     if (!file_exists($smallPath)) {
         mkdir($smallPath, 0777, true);
     }
