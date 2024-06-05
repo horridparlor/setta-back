b2a26cbfc00648cfc96f8bfb70889d31859f25a9
Eero Laine <horridparlormaster@gmail.com>
2024-06-05 23:08:46 +0300

    Don't show old erratas to free users

--- api/user/cards.php
+++ api/user/cards.php
@@ -13,9 +13,26 @@ function getCards(Database $database): string
     $user = $database->getUser();
 
     $sql = SELECT_CARD . <<<SQL
+        LEFT JOIN (
+            SELECT id, errataOfId
+            FROM card
+            WHERE created_at IN (
+                SELECT MAX(created_at)
+                FROM card
+                GROUP BY errataOfId
+            )
+        ) newestErrata ON newestErrata.errataOfId = card.errataOfId
         WHERE card.isDeleted = 0
+        AND (
+            newestErrata.id IS NULL
+            OR expansion.isReleased = 0
+            OR card.id = newestErrata.id
+            OR card.ownerId = :userId
+        )
     SQL;
-    $replacements = array();
+    $replacements = array(
+        'userId' => ['value' => $user ? $user->getId() : 0, 'type' => PDO::PARAM_INT]
+    );
     if (!$user) {
         $sql .= <<<SQL
             AND (expansionOwner.isAdmin = 1 AND expansion.isReleased = 1)
@@ -28,7 +45,6 @@ function getCards(Database $database): string
                 OR expansionOwner.id = :userId
             )
         SQL;
-        $replacements['userId'] = ['value' => $user->getId(), 'type' => PDO::PARAM_INT];
     }
     $cards = $database->query($sql, $replacements);
     return $database->responseSuccess(array(
